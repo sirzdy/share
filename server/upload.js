8264ec8a41f1943d0f31922228e34d5158afc01e
zdy <boxuerixin@hotmail.com>
2018-08-09 18:12:33 +0800

    更新上传插件

--- server/upload.js
+++ server/upload.js
@@ -3,7 +3,10 @@ const multer = require('multer');
 const path = require('path');
 const app = express();
 exports.upload = function (opts) {
-    let { port, root } = opts;
+    let {
+        port,
+        root
+    } = opts;
     return new Promise((resolve, reject) => {
         let storage = multer.diskStorage({
             destination: function (req, file, cb) {
@@ -14,21 +17,44 @@ exports.upload = function (opts) {
             }
         });
 
-        let upload = multer({ storage });
-
-        app.get('/', (req, res) => {
-            res.sendFile(path.join(__dirname, 'index.html'));
-        })
-
-        app.post('/upload', upload.array('files'), function (req, res, next) {
-            let files = req.files;
-            let rets = [];
-            for (let i = 0; i < files.length; i++) {
-                let file = files[i];
-                rets.push(file);
-            }
-            res.json({ rets });
+        let upload = multer({
+            storage
         });
+        app.use(express.static(path.join(__dirname, 'web')));
+        app.get('/', (req, res) => {
+            res.sendFile(path.join(__dirname, 'web/index.html'));
+        })
+        let up = upload.array('files');
+        app.post('/upload', function (req, res) {
+            up(req, res, function (err) {
+                if (err) {
+                    res.status(500).json({
+                        error: err
+                    })
+                    return
+                }
+                let files = req.files;
+                let rets = [];
+                for (let i = 0; i < files.length; i++) {
+                    let file = files[i];
+                    rets.push(file);
+                }
+                res.json({
+                    rets
+                });
+            })
+        })
+        // app.post('/upload', upload.array('files'), function (req, res, next) {
+        //     let files = req.files;
+        //     let rets = [];
+        //     for (let i = 0; i < files.length; i++) {
+        //         let file = files[i];
+        //         rets.push(file);
+        //     }
+        //     res.json({
+        //         rets
+        //     });
+        // });
 
         app.listen(port, () => resolve(port))
     })
